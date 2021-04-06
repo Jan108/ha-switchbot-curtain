a16b807bf6ab0651b72a82d1063d46218cd3fda1
Jan Braker <braker.jan@icloud.com>
2021-04-06 23:03:00 +0200

    feature: reading states
    feature: reading states of battery level, position and light level with updated lib

--- README.md
+++ README.md
@@ -6,15 +6,20 @@ Controls switchbot curtain using Home Assistant
 Supported operations:
   - open / close / stop
   - set position [0-100]%
-  
+  - read switchbot sensors / position  
+
 Not supported:
-  - read switchbot sensors / position
   - password
 
-## Find mac address of your device on linux
+## Find mac address of your device
+
+### On linux
 ```
 sudo hcitool lescan
 ```
+
+### In Switchbot app
+HOME -> YOUR CURTAIN -> More -> Setting (Top right) -> Three dots (Top right)
 
 ## Installation
 
@@ -22,11 +27,25 @@ sudo hcitool lescan
 2. Add the config to your configuration.yaml file as explained below.
 3. Restart Home Assistant
 
-## Example
+## Config
+
+### Description
+
+  - ```mac``` The MAC address of your curtain.
+  - ```name``` (Optional) The name of the Switchbot Curtain.
+  - ```reverse``` (Optional, Default: False) Set this to True if your curtain mode is calibrated to 'Open from left to right'.
+  - ```time_between_update_command``` (Optional, Default: 10) Time your curtain needs to move from fully closed to fully opened. 
+  - ```scan_interval``` (Optional, Default: 3600) Interval Home Assistant updates the states.
+
+### Example
 ```
 cover:
   - platform: switchbot-curtain
     mac: XX:XX:XX:XX:XX:XX
+    name: "Curtain living room"
+    reverse: True
+    time_between_update_command: 11
+    scan_interval: 4200
 ```
 
 ## Debugging problems
--- custom_components/switchbot-curtain/cover.py
+++ custom_components/switchbot-curtain/cover.py
@@ -1,12 +1,14 @@
 """Support for Switchbot Curtain."""
 from typing import Any, Dict
+from datetime import timedelta
 
 # pylint: disable=import-error, no-member
-import switchbot
+from . import SwitchbotCurtain
 import voluptuous as vol
 import logging
 
-from homeassistant.const import CONF_MAC, CONF_NAME, CONF_PASSWORD, STATE_OPEN, STATE_CLOSED, STATE_OPENING, STATE_CLOSING
+from homeassistant.const import CONF_MAC, CONF_NAME, CONF_PASSWORD, STATE_OPEN, STATE_CLOSED, STATE_OPENING, \
+    STATE_CLOSING
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.restore_state import RestoreEntity
 
@@ -16,26 +18,19 @@ from homeassistant.components.cover import (
     SUPPORT_OPEN, SUPPORT_CLOSE, SUPPORT_STOP, SUPPORT_SET_POSITION,
 )
 
-
-OPEN_KEY      = '570f450105ff00'  #570F4501010100
-CLOSE_KEY     = '570f450105ff64'  #570F4501010164
-POSITION_KEY  = '570F450105ff' # +actual_position ex: 570F450105ff32 for 50%
-STOP_KEY      = '570F45010001'
-
-
-SWITCHBOT_WAIT_SEC = 10 #seconds
-BLE_RETRY_COUNT = 5
-
 # Initialize the logger
 _LOGGER = logging.getLogger(__name__)
 
 DEFAULT_NAME = "Switchbot Curtain"
+SCAN_INTERVAL = timedelta(seconds=3600)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
         vol.Required(CONF_MAC): cv.string,
         vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
         vol.Optional(CONF_PASSWORD): cv.string,
+        vol.Optional("reverse", default=False): cv.boolean,
+        vol.Optional("time_between_update_command", default=10): cv.positive_int
     }
 )
 
@@ -45,21 +40,24 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     name = config.get(CONF_NAME)
     mac_addr = config[CONF_MAC]
     password = config.get(CONF_PASSWORD)
-    add_entities([SwitchBotCurtain(mac_addr, name, password)])
+    reverse = config.get("reverse")
+    time_between_update_command = config.get("time_between_update_command")
+    add_entities([SwitchBotCurtain(mac=mac_addr, name=name, password=password, reverse_mode=reverse,
+                                   time_between_update_command=time_between_update_command)], True)
 
 
 class SwitchBotCurtain(CoverEntity, RestoreEntity):
     """Representation of a Switchbot."""
 
-    def __init__(self, mac, name, password) -> None:
+    def __init__(self, mac, name, password, reverse_mode=False, time_between_update_command=10) -> None:
         """Initialize the Switchbot."""
 
         self._state = None
         self._last_run_success = None
         self._name = name
         self._mac = mac
-        self._device = switchbot.Switchbot(mac=mac, password=password)
-        self._pos = 0
+        self._device = SwitchbotCurtain(mac=mac, password=password, reverse_mode=reverse_mode,
+                                        time_between_update_command=time_between_update_command)
 
     async def async_added_to_hass(self):
         """Run when entity about to be added."""
@@ -70,14 +68,24 @@ class SwitchBotCurtain(CoverEntity, RestoreEntity):
         _LOGGER.info('Switchbot state %s', state)
         self._state = state.state
 
-        if 'current_position' in state.attributes:
-            self._pos = state.attributes["current_position"]
-        
-
     @property
     def assumed_state(self) -> bool:
         """Return true if unable to access real state of entity."""
+        return False
+
+    @property
+    def should_poll(self) -> bool:
+        """Return True if entity has to be polled for state.
+
+        False if entity pushes its state to HA.
+        """
         return True
+
+    def update(self):
+        """Do some stuff"""
+        _LOGGER.info('Switchbot %s updating', self._mac)
+        self._device.update()
+        _LOGGER.info('Switchbot %s updating done', self._mac)
 
     @property
     def unique_id(self) -> str:
@@ -92,7 +100,10 @@ class SwitchBotCurtain(CoverEntity, RestoreEntity):
     @property
     def device_state_attributes(self) -> Dict[str, Any]:
         """Return the state attributes."""
-        return { "last_run_success": self._last_run_success }
+        return {"last_run_success": self._last_run_success,
+                "battery": self._device.get_battery_percent(),
+                "light_level": self._device.get_light_level(),
+                "reversed": self._device.is_reversed()}
 
     @property
     def device_class(self) -> str:
@@ -107,7 +118,7 @@ class SwitchBotCurtain(CoverEntity, RestoreEntity):
     @property
     def is_closed(self):
         """Return if the cover is closed."""
-        return self.current_cover_position <= 0
+        return (self.current_cover_position >= 100) if self._device.is_reversed() else True
 
     def open_cover(self, **kwargs) -> None:
         """Open the curtain with using this device."""
@@ -115,12 +126,7 @@ class SwitchBotCurtain(CoverEntity, RestoreEntity):
         _LOGGER.info('Switchbot to open curtain %s...', self._mac)
 
         """Open curtain"""
-        if self._device._sendcommand(OPEN_KEY, BLE_RETRY_COUNT):
-            self._last_run_success = True
-            self._pos = 100
-        else:
-            self._last_run_success = False
-        
+        self._last_run_success = self._device.set_position(0)
 
     def close_cover(self, **kwargs) -> None:
         """Close the curtain with using this device."""
@@ -128,11 +134,7 @@ class SwitchBotCurtain(CoverEntity, RestoreEntity):
         _LOGGER.info('Switchbot to close the curtain %s...', self._mac)
 
         """Close curtain"""
-        if self._device._sendcommand(CLOSE_KEY, BLE_RETRY_COUNT):
-            self._last_run_success = True
-            self._pos = 0
-        else:
-            self._last_run_success = False
+        self._last_run_success = self._device.set_position(100)
 
     def stop_cover(self, **kwargs) -> None:
         """Stop the moving of this device."""
@@ -140,26 +142,16 @@ class SwitchBotCurtain(CoverEntity, RestoreEntity):
         _LOGGER.info('Switchbot to stop %s...', self._mac)
 
         """Stop curtain"""
-        if self._device._sendcommand(STOP_KEY, BLE_RETRY_COUNT):
-            self._last_run_success = True
-        else:
-            self._last_run_success = False
-        
+        self._last_run_success = self._device.stop()
 
     def set_cover_position(self, **kwargs):
         """Move the cover shutter to a specific position."""
         position = kwargs.get(ATTR_POSITION)
-        hexPosition = "%0.2X" % (100 - position) # curtain position in reverse mode
-        
+
         _LOGGER.info('Switchbot to move at %d %s...', position, self._mac)
-        
-        if self._device._sendcommand(POSITION_KEY + hexPosition, BLE_RETRY_COUNT):
-            self._pos = position
-            self._last_run_success = True
-        else:
-            self._last_run_success = False
+        self._last_run_success = self._device.set_position(position)
 
     @property
     def current_cover_position(self):
         """Return the current position of cover shutter."""
-        return self._pos
+        return self._device.get_position()
